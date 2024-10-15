6fde8492013ce9e5234c1607ba4db588f3827cde
Shaun <Hundin231@gmail.com>
2024-10-15 22:48:25 +0800

    Add skeleton three.js react website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Testing developer Portfolio
+      </div>
+    </>
+  )
+}
+
+export default App
